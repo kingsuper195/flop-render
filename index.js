f924b496bb9a77660405c7f8a34cdc933e0219bf
Peter Durdin <peter.durdin.superworld@gmail.com>
2024-07-16 20:45:11 +1000

    add updateSkin

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 
 const FPS = 30;
-const RENDER_STEP_TIME = 1000/FPS;
+const RENDER_STEP_TIME = 1000 / FPS;
 
 export class RenderLoop {
   sprites = [];
   callbacks = [];
   zero = document.timeline.currentTime;
   step = 0;
+  fps = FPS;
 
   constructor(renderer) {
     this.renderer = renderer;
@@ -24,7 +25,7 @@ export class RenderLoop {
 
   frame() {
     // Run callbacks and steps once per frame
-    if((document.timeline.currentTime - this.zero) / RENDER_STEP_TIME >= this.step) {
+    if ((document.timeline.currentTime - this.zero) / RENDER_STEP_TIME >= this.step) {
       this.callbacks.forEach(callback => callback(this));
       this.drawStep();
       this.step++;
@@ -37,5 +38,39 @@ export class RenderLoop {
       this.renderer.updateDrawableProperties(sprite.render, sprite.getRendererProps())
     );
     this.renderer.draw();
+  }
+
+  updateSkin(sprite) {
+    const WantedSkinType = {
+      bitmap: 'bitmap',
+      vector: 'vector',
+    };
+    const wantedSkin = sprite.currentCostume.type;
+
+    // Bitmap (squirrel)
+    const image = new Image();
+    image.addEventListener('load', () => {
+      const bitmapSkinId = renderer.createBitmapSkin(image);
+      if (wantedSkin === WantedSkinType.bitmap) {
+        this.renderer.updateDrawableProperties(sprite.render, {
+          skinId: bitmapSkinId
+        });
+      }
+    });
+    image.crossOrigin = 'anonymous';
+    image.src = sprite.currentCostume.data;
+
+    // SVG (cat 1-a)
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', () => {
+      const skinId = renderer.createSVGSkin(xhr.responseText);
+      if (wantedSkin === WantedSkinType.vector) {
+        this.renderer.updateDrawableProperties(sprite.render, {
+          skinId: skinId
+        });
+      }
+    });
+    xhr.open('GET', sprite.currentCostume.data);
+    xhr.send();
   }
 }
