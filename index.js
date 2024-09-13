6d10a8f23447777fca5bc1b58af00549102db58d
Peter Durdin <peter.durdin.superworld@gmail.com>
2024-09-13 13:25:40 +0700

    fix render is not defined

--- index.js
+++ index.js
@@ -1,53 +1,8 @@
 
 const FPS = 30;
 const RENDER_STEP_TIME = 1000 / FPS;
-const stage = renderer.createDrawable('group');
-renderer.updateDrawableProperties(stage, {
-    position: [0, 0],
-    scale: [100, 100],
-    direction: 90
-});
-export function setStage(x, wantedSkin) {
-  return new Promise((resolve) => {
 
-    const WantedSkinType = {
-      bitmap: 'bitmap',
-      vector: 'vector',
-    };
 
-    // Bitmap
-    if (wantedSkin === WantedSkinType.bitmap) {
-      const image = new Image();
-      image.addEventListener('load', () => {
-        const bitmapSkinId = this.renderer.createBitmapSkin(image);
-
-        this.renderer.updateDrawableProperties(sprite.render, {
-          skinId: bitmapSkinId
-        });
-
-        resolve();
-      });
-      image.crossOrigin = 'anonymous';
-      image.src = x;
-    }
-
-    // SVG
-    if (wantedSkin === WantedSkinType.vector) {
-      const xhr = new XMLHttpRequest();
-      xhr.addEventListener('load', () => {
-        const skinId = this.renderer.createSVGSkin(xhr.responseText);
-
-        this.renderer.updateDrawableProperties(sprite.render, {
-          skinId: skinId
-        });
-
-        resolve();
-      });
-      xhr.open('GET', x);
-      xhr.send();
-    }
-  });
-}
 export class RenderLoop {
   sprites = [];
   callbacks = [];
@@ -58,6 +13,55 @@ export class RenderLoop {
   constructor(renderer) {
     this.renderer = renderer;
     requestAnimationFrame(this.frame.bind(this));
+  }
+
+
+
+  setStage(x, wantedSkin) {
+    return new Promise((resolve) => {
+      stage = renderer.createDrawable('group');
+       this.renderer.updateDrawableProperties(stage, {
+        position: [0, 0],
+        scale: [100, 100],
+        direction: 90
+      });
+      const WantedSkinType = {
+        bitmap: 'bitmap',
+        vector: 'vector',
+      };
+
+      // Bitmap
+      if (wantedSkin === WantedSkinType.bitmap) {
+        const image = new Image();
+        image.addEventListener('load', () => {
+          const bitmapSkinId = this.renderer.createBitmapSkin(image);
+
+          this.renderer.updateDrawableProperties(sprite.render, {
+            skinId: bitmapSkinId
+          });
+
+          resolve();
+        });
+        image.crossOrigin = 'anonymous';
+        image.src = x;
+      }
+
+      // SVG
+      if (wantedSkin === WantedSkinType.vector) {
+        const xhr = new XMLHttpRequest();
+        xhr.addEventListener('load', () => {
+          const skinId = this.renderer.createSVGSkin(xhr.responseText);
+
+          this.renderer.updateDrawableProperties(sprite.render, {
+            skinId: skinId
+          });
+
+          resolve();
+        });
+        xhr.open('GET', x);
+        xhr.send();
+      }
+    });
   }
 
   addSprite(sprite) {
